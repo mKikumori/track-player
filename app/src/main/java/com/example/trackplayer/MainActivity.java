fc88b392b3fa9a5c81ff6956dfd46b4c5e036b61
mKikumori <matheuskiku@gmail.com>
2024-09-12 13:24:33 +1000

    testing

--- app/src/main/java/com/example/trackplayer/MainActivity.java
+++ app/src/main/java/com/example/trackplayer/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.Toast;
 
+// Test
 public class MainActivity extends AppCompatActivity {
 
     private MediaPlayer mediaPlayer;
